172bdab910aa70a5f790d11e9b87b9d249df73c8
David Manouchehri <david.manouchehri@ai.moda>
2025-01-30 19:33:21 +0000

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export interface Env {
+	ENVIRONMENT: string;
+}
+
+export default {
+	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
+		console.debug("invoking cron");
+
+		// ctx.waitUntil();
+	}
+};
